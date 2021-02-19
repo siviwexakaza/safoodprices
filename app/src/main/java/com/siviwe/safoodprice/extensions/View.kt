6500e9f6d65a78620fb7762563575a3e5dce52fb
siviwe <siviwe@fuzzelogicsolutions.com>
2021-02-19 10:01:12 +0200

    Impl Glide

--- app/src/main/java/com/siviwe/safoodprice/extensions/View.kt
+++ app/src/main/java/com/siviwe/safoodprice/extensions/View.kt
@@ -1,6 +1,11 @@
 package com.siviwe.safoodprice.extensions
 
 import android.view.View
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.siviwe.safoodprice.R
 
 fun View.hide(keepSpace: Boolean = false) {
     this.visibility = if (keepSpace) View.INVISIBLE else View.GONE
@@ -16,4 +21,14 @@ fun View.visibleOrGone(isVisible: Boolean) {
 
 fun View.visibleOrInvisible(isVisible: Boolean) {
     this.visibility = if (isVisible) View.VISIBLE else View.INVISIBLE
+}
+
+fun ImageView.loadImage(uri: String, progress: CircularProgressDrawable){
+    val options = RequestOptions()
+            .placeholder(progress)
+            .error(R.drawable.ic_baseline_close_24)
+    Glide.with(context)
+            .setDefaultRequestOptions(options)
+            .load(uri)
+            .into(this)
 }
--- app/src/main/java/com/siviwe/safoodprice/view/adapter/ProductAdapter.kt
+++ app/src/main/java/com/siviwe/safoodprice/view/adapter/ProductAdapter.kt
@@ -3,10 +3,14 @@ package com.siviwe.safoodprice.view.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.siviwe.safoodprice.R
+import com.siviwe.safoodprice.extensions.loadImage
 import com.siviwe.safoodprice.providers.Product
+import com.siviwe.safoodprice.utils.getProgressDrawable
 import java.util.zip.Inflater
 
 class ProductAdapter(val products: ArrayList<Product>): RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
@@ -32,5 +36,7 @@ class ProductAdapter(val products: ArrayList<Product>): RecyclerView.Adapter<Pro
         holder.view.findViewById<TextView>(R.id.productName).text = products[position].name
         holder.view.findViewById<TextView>(R.id.productPrice).text = products[position].price
         holder.view.findViewById<TextView>(R.id.productStore).text = products[position].store
+        holder.view.findViewById<ImageView>(R.id.productImage)
+                .loadImage(products[position].imageURL!!, getProgressDrawable(holder.view.findViewById<ImageView>(R.id.productImage).context))
     }
 }
